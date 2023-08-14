13f779b7ddb89d2c49dfd336d56819c0ab435ec9
wnal4634 <wnal4634@naver.com>
2023-08-14 23:25:49 +0900

    숫자 카운트

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import confetti from "https://esm.run/canvas-confetti@1";
+import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+    let [count, setCount] = useState(0);
     function onClick() {
+        setCount(count + 1);
         confetti({
             particleCount: 150,
-            spread: 60,
+            spread: 90,
         });
     }
 
     return (
         <div>
-            <button className="button" onClick={onClick}>
+            <button className={styles.button} onClick={onClick}>
                 <span>🎉</span>
                 <span>Like</span>
             </button>
+            <div>{count}번의 축하를 받았습니다!</div>
         </div>
     );
 }
